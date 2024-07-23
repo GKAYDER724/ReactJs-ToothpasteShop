081f3b599a285ebb5920ca55517c7a54bc832538
GKAYDER724 <145838116+GKAYDER724@users.noreply.github.com>
2024-07-23 10:58:34 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import React, { Suspense, useEffect } from 'react'
 import { CSpinner, useColorModes } from '@coreui/react'
-import './scss/style.scss'
-import React from "react";
+// import './scss/style.scss'
 import Home from "./clients/pages/Home";
 import About from "./clients/pages/About";
 import Contact from "./clients/pages/Contact";
@@ -11,7 +10,6 @@ import Product from "./clients/pages/Product";
 import Notfound from "./clients/pages/404";
 import Navbar from "./clients/components/Navbar";
 import Footer from "./clients/components/Footer";
-import { Route, Routes } from "react-router-dom";
 import ProductDetails from "./clients/components/ProductDetails";
 import Cart from "./clients/pages/Cart";
 
@@ -19,10 +17,10 @@ import Cart from "./clients/pages/Cart";
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
 // Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const Login = React.lazy(() => import('./admin/views/pages/login/Login'))
+const Register = React.lazy(() => import('./admin/views/pages/register/Register'))
+const Page404 = React.lazy(() => import('./admin/views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./admin/views/pages/page500/Page500'))
 
 const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import 'core-js'
-import App from './App'
 import store from './store'
 import React from "react";
 import ReactDOM from "react-dom/client";
