f738521d9a25bcfe9a0fac6cf5f878d3a4a0e026
vando09 <145195909+vando09@users.noreply.github.com>
2024-08-15 18:28:39 +0700

    asm2

--- ASM-React/src/views/sagas/index.js
+++
@@ -1,7 +0,0 @@
-// import { all } from 'redux-saga/effects'
-// import watchCategoryActions from './categorySagas'
-
-// function* rootSaga() {
-//   yield all([watchCategoryActions()])
-// }
-// export default rootSaga
