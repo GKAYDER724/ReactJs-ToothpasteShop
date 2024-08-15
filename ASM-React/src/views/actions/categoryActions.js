6872b7de2e6293d5328a4a82a3c84d52c7f038f3
vando09 <145195909+vando09@users.noreply.github.com>
2024-08-15 16:59:35 +0700

    asm2

---
+++ ASM-React/src/views/actions/categoryActions.js
@@ -0,0 +1,28 @@
+// export const fetchCategoriesRequest = () => ({
+//   type: 'FETCH_CATEGORIES_REQUEST',
+// })
+
+// export const fetchCategoriesSuccess = (categories) => ({
+//   type: 'FETCH_CATEGORIES_SUCCESS',
+//   payload: categories,
+// })
+
+// export const fetchCategoriesFailure = (error) => ({
+//   type: 'FETCH_CATEGORIES_FAILURE',
+//   payload: error,
+// })
+
+// export const deleteCategoryRequest = (id) => ({
+//   type: 'DELETE_CATEGORY_REQUEST',
+//   payload: id,
+// })
+
+// export const deleteCategorySuccess = (id) => ({
+//   type: 'DELETE_CATEGORY_SUCCESS',
+//   payload: id,
+// })
+
+// export const deleteCategoryFailure = (error) => ({
+//   type: 'DELETE_CATEGORY_FAILURE',
+//   payload: error,
+// })
